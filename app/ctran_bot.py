2df8c0133189c2da0279a402b1eeb3033113b7a5
BrokenShell <webmaster@sharpdesigndigital.com>
2023-09-09 12:58:04 -0700

    adds module 4, objective 3

--- app/ctran_bot.py
+++ app/ctran_bot.py
@@ -8,7 +8,8 @@ from ctransformers import AutoModelForCausalLM
 
 def generate_story(user_prompt: str):
     system_prompt = "You are a masterful storyteller. Please respond with a " \
-                    "short story based on the following prompt."
+                    "short story based on the following prompt. Please respond " \
+                    "with just the story."
     prompt = f"### Instruction: {system_prompt}\n\n" \
              f"{user_prompt}\n\n" \
              f"### Response:\n"
@@ -16,8 +17,9 @@ def generate_story(user_prompt: str):
         model_path_or_repo_id="./models/openorca-platypus2-13b.Q4_K_M.gguf",
         model_type="llama",
         context_length=4096,
-        max_new_tokens=4000,
-        temperature=0.5,
+        max_new_tokens=2048,
+        temperature=0.8,
+        stop=["###"],
     )
     reply = llm(prompt)
     return reply
